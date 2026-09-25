b39d0b1f53e64b6312375057f1a49d3ee727e6ac
Connor Smith <phoenixvine32908@gmail.com>
2026-09-24 21:40:30 -0400

    Versioning

--- src/main/java/net/phoenixvine/guilds/integration/gtceu/GuildOwnerTypeRegistrar.java
+++ src/main/java/net/phoenixvine/guilds/integration/gtceu/GuildOwnerTypeRegistrar.java
@@ -4,7 +4,11 @@ import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.common.machine.owner.RegisterOwnerTypeEvent;
 
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.phoenixvine.guilds.PhoenixGuilds;
 
+
+@Mod.EventBusSubscriber(modid = PhoenixGuilds.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public final class GuildOwnerTypeRegistrar {
 
     private GuildOwnerTypeRegistrar() {}
